c99265b6df3e589f19cd5ba754fff982f2c59973
unknown <nenegrakrina@gmail.com>
2021-07-07 20:20:30 +0300

    alert component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AlertContext } from '../context/alert/AlertContext'
 
 export const Search = () => {
+  const {show} = useContext(AlertContext)
+
+  const onSubmit = event => {
+    if (event.key === 'Enter') {
+      show('This is alert!')
+    }
+  }
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        onKeyPress={onSubmit}
       />
     </div>
   )
